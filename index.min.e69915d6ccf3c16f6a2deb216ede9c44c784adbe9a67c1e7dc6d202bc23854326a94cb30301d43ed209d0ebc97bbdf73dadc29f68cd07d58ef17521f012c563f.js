d446fd8d6ac48a5e61dc974ad4a6a6d057069384
jflann <jflann@users.noreply.github.com>
2022-09-16 02:39:56 +0000

    deploy: c50ad3f24fea97b0589ef06f4f46a7ad188c89c4

--- index.min.e69915d6ccf3c16f6a2deb216ede9c44c784adbe9a67c1e7dc6d202bc23854326a94cb30301d43ed209d0ebc97bbdf73dadc29f68cd07d58ef17521f012c563f.js
+++ index.min.e69915d6ccf3c16f6a2deb216ede9c44c784adbe9a67c1e7dc6d202bc23854326a94cb30301d43ed209d0ebc97bbdf73dadc29f68cd07d58ef17521f012c563f.js
@@ -730,31 +730,33 @@ Numerous other improvements followed including:
 Changing the gauge to represent standard gauge (1435 mm), which approximates to 12 pixels in absolute terms. Changing the track spacing from 4 meters to 4.5 meters. The founder of the project, Swordmaster, determined the best way to make realistic railways by spacing out the tracks with the new gauge which makes the outer rails run flush with the Maxis rails but not with the inner rails.
 The following is a snapshot from the initial 2013 RealRailway documentation which demonstrates the rationale for the shift to the RRW standard:
 Notice how the road pieces line up with the real life road networks but the Maxis Rail does not!
-RealRailway was at first basic in appearance and functionality. However, further enhancements have led to RRW surpassing the functionality of Maxis rail in the NAM.
+At first, RealRailway was basic in appearance and functionality. However, further enhancements have led to RRW surpassing the functionality of Maxis rail in the NAM.
 Basic Usage # RealRailway is a complete replacement of the rail network. Basic double-track rail (DTR) is drawn using the rail tool and advanced functionality is achieved with draggable patterns or ploppable FLEX pieces. Single-track rail (STR) is provided as an override network and can initiated by a dedicated starter or by certain FLEX turnouts. RRW also covers legacy DTR puzzle pieces and wide radius curves.
 FlexTrack # NAM 33 delivered a suite of new functionality to the RealRailway known as FlexTrack. This advancement allowed for medium to large wide radius curves rechristened as multi-radius curves (MRC). Under the MRC nomenclature, sizes range from the smallest existing minicurves at R1 to large, sweeping curves at R5. Patterns were devised to allow the curves to drawn out with the rail tool without having to go into the puzzle piece tab loop. R1, R2 and R3 can also be made into turnouts by drawing orthogonally from the orthogonal end while there is support for diagonal turnouts with R1 and R3. R4 and R5 do not support any turnouts with there sole function to be curves! Also a range of new turnout options were introduced with the RealRailway FlexTrack component. This allowed for new turnouts in tight situations where the former parts where not applicable or accessible. FlexTrack improvements were spearheaded by eggman121 who developed the new components building on the base provided by Swordmaster.
 The FlexTrack currently relies on patterns to make the desired curves or turnouts/ switches. While there is the advantage of not having to go into the menu to find the pieces they can be hard to find the right patterns with conflicts if the pieces are too close to each other. Testing the capabilities in a blank region is always helpful to get the right patterns for the FlexTrack component.
-The patterns and resultants are shown below\u0026hellip;.
-Curves # Fractional Angle Railroad (FARR) # Fractional Angle Rail Road (FARR) as it will be referenced from now is the angled sections of track that conform to three angles to compiment the orthogonal (0 degrees) and diagional (45 degrees). They are FARR-3 (18.43 degrees), FARR-2 (26.56 degrees) and FARR-1.5 (33.69 degrees). At the time of writing this document the FARR is only able to be drawn out using the rail tool. Flex pieces for these sections of track are anticipated but will arrive in a future edition of the NAM.
+The patterns and resultants are shown below.
+Curves # Fractional Angle Railroad (FARR) # Fractional Angle Rail Road (FARR) is the angled sections of track that conform to three angles which compiment the orthogonal (0 degrees) and diagional (45 degrees). They are FARR-3 (18.43 degrees), FARR-2 (26.56 degrees) and FARR-1.5 (33.69 degrees). At the time of writing this document the FARR is only able to be drawn out using the rail tool. Flex pieces for these sections of track are anticipated but will arrive in a future edition of the NAM.
 Patterns are shown below\u0026hellip;
-FARR-3
-FARR-3 Stright
+FARR-3 # FARR-3 Straight
 FARR-3 to Orthogonal
 FARR-3 to Diagonal
 FARR-3 to FARR-2
-FARR-2
-FARR-2 Stright FARR-2 to Orthogonal
+FARR-2 # FARR-2 Straight
+FARR-2 to Orthogonal
 FARR-2 to Diagonal
-FARR-2 to FARR-1.5/1.33
-FARR-2 G2 Tunout
-FARR-1.5/1.33
-FARR-1.5/1.33 Stright
-FARR-1.5/1.33 to Orthogonal
-FARR-1.5/1.33 to Diagonal
-FARR-1.5/1.33 J2 Tunout
-Turnouts # The turnouts and switches come in all sorts of forms and there are a range of turnouts to use. They range from A1 Turnouts to large B2 extended turnouts as well. The set classes are shown in the table below.Not all the turnouts have been made. Some of the possibilities will need flex implementation to be realised.The table shown is just an example of the proposed turnouts.Additional turnouts have been made possible however for the Real Railway.
-A Class Turnouts # B Class Turnouts # C Class Turnouts # D Class Turnouts # E Class Turnouts # The FARR-2 Alt has the same angle as the normal FARR-2 but has a different base and pattern. Also the geometry is slightly different and can make complex turnouts as shown further in the document. The FARR-2 Alternative implementation is considered a FlexTrack item.Below are the various setups.The base for the FARR-2 Alt is the zig zag base and is designed for flat surfaces only. This section of track has no tolerance for slopes!
-The small complex turnouts compliments the FARR-2 Alt with turnout setups that cross double track rail DTR with both single track rail STR and DTR variants. Upgrading to DTR involves drawing through the middle pointsas shown by the red arrow.
+FARR-2 to FARR-1.5
+FARR-2 G2 Turnout
+FARR-1.5 # FARR-1.5 Straight
+FARR-1.5 to Orthogonal
+FARR-1.5 to Diagonal
+FARR-1.5 J2 Turnout
+FARR-2 Alt # The FARR-2 Alt has the same angle as the normal FARR-2 but has a different base and pattern. Also the geometry is slightly different and can make complex turnouts as shown further in the document. The FARR-2 Alternative implementation is considered a FlexTrack item. Below are the various setups. The base for the FARR-2 Alt is the zig zag base and is designed for flat surfaces only. This section of track has no slope tolerance.
+FARR-2 Alt Straight
+FARR-2 Alt to Orthogonal
+FARR-2 Alt to Diagonal
+FARR-2 Alt G2 Turnout
+Turnouts # The turnouts and switches come in all sorts of forms and there are a range of turnouts to use. They range from A1 Turnouts to large B2 extended turnouts as well. The set classes are shown in the table below. Not all the turnouts have been made. Some of the possibilities will need flex implementation to be realised. The table shown is just an example of the proposed turnouts. Additional turnouts have been made possible however for the Real Railway.
+A Class Turnouts # B Class Turnouts # C Class Turnouts # D Class Turnouts # E Class Turnouts # The small complex turnouts compliments the FARR-2 Alt with turnout setups that cross double track rail DTR with both single track rail STR and DTR variants. Upgrading to DTR involves drawing through the middle pointsas shown by the red arrow.
 Elevated RealRailway (ERRW) # Elevated RealRailway (ERRW) is the draggable implementation of heavy rail viaducts. ERRW is implemented at both L1 (7.5m) and L2 (15.5m). Both variants include support for some minicurves and multi radius curves. The MRCs supported are Radius (1, 2 and 3).
 Height Transitions # Switching between ground level (L0) RRW and ERRW is accomplished using FLEX transition pieces found in the Draggable Elevated Railways (ERRW) menu.
 Usage Notes # The rail is not to be drawn through the transition but to the stub at each end shown in the pictures below\u0026hellip;
@@ -775,7 +777,7 @@ B2 Turnout
 A2 Wye Turnout
 Crossings # When crossing other networks a rail piece MUST! be placed on the concerning tile if you are crossing a non-rail network. This is due to the fact that rail code must be present in the network. Level crossing (LX) overridden by the ERRW are exempt however. A picture of what you should do is shown below\u0026hellip;
 Of course this action is not needed if the network being crossed is the Rail network itself.
-Credits # The following members contributed to the development of the RealRailway:
+Crossing Support # L1 # Network OxO OxD DxO DxD Street Yes No No No Road Yes No No No One Way Road Yes No No No Avenue Yes No No No Maxis Highway Yes No No No Real Railway DTR Yes No No Yes Real Railway STR Yes No No Yes MIS Yes No No No RHW-2 Yes No No No RHW-3 Partial No No No RHW-4S Yes No No No RHW-6S Yes No No No RHW-6C Yes No No No RHW-8S Yes No No No RHW-8C Yes No No No RHW-10S Yes No No No TLA3 Yes No No No AVE2 Yes No No No OWR-1 Yes No No No OWR-3 Yes No No No NRD-4 Yes No No No TLA-5 Yes No No No OWR-4 Yes No No No OWR-5 Yes No No No RD-4 Yes No No No RD-6 Yes No No No TLA-7 Yes No No No AVE-6 Yes No No No GLR Sets Yes No No No TOS Partial No No No TOR Partial No No No TIA Partial No No No SAM Sets Yes No No No GHSR Yes No No No L2 # Network OxO OxD DxO DxD Street Yes No No No Road Yes No No No One Way Road Yes No No No Avenue Yes No No No Maxis Highway Yes No No No Real Railway DTR Yes No No Yes Real Railway STR Yes No No Yes MIS Yes No No No RHW-2 Yes No No No RHW-3 Partial No No No RHW-4S Yes No No No RHW-6S Yes No No No RHW-6C Yes No No No RHW-8S Yes No No No RHW-8C Yes No No No RHW-10S Yes No No No TLA3 Yes No No No AVE2 Yes No No No OWR-1 Yes No No No OWR-3 Yes No No No NRD-4 Yes No No No TLA-5 Yes No No No OWR-4 Yes No No No OWR-5 Yes No No No RD-4 Yes No No No RD-6 Yes No No No TLA-7 Yes No No No AVE-6 Yes No No No GLR Sets Yes No No No TOS Partial No No No TOR Partial No No No TIA Partial No No No SAM Sets Yes No No No GHSR Yes No No No Credits # The following members contributed to the development of the RealRailway:
 Swordmaster eggman121 mgb204/rsc204 woodb3kmaster Tarkus memo Shadow Assassin rivit Simmer2 flann `}),e.add({id:28,href:"/docs/feature-guides/road-viaducts/",title:"Road Viaducts",description:"This documentation has not yet been written.",content:`This documentation has not yet been written.
 `}),e.add({id:29,href:"/docs/feature-guides/roundabouts/",title:"Roundabouts",description:"This documentation has not yet been written.",content:`This documentation has not yet been written.
 `}),e.add({id:30,href:"/docs/feature-guides/rural-roads/",title:"Rural Roads",description:"This documentation has not yet been written.",content:`This documentation has not yet been written.
