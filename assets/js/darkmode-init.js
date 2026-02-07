e85bf9b27f3d21749b40a3838a4f41b9a069a470
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2026-02-06 21:33:17 -0500

    Set boostrap dark theme correcly on site load
    If dark theme is preferred, also initialize bootstrap theme as dark

--- assets/js/darkmode-init.js
+++ assets/js/darkmode-init.js
@@ -5,17 +5,20 @@ if (globalDark && (localMode === null)) {
 
   localStorage.setItem('theme', 'dark');
   document.documentElement.setAttribute('data-dark-mode', '');
+  document.documentElement.setAttribute('data-bs-theme', 'dark');
 
 }
 
 if (globalDark && (localMode === 'dark')) {
 
   document.documentElement.setAttribute('data-dark-mode', '');
+  document.documentElement.setAttribute('data-bs-theme', 'dark');
 
 }
 
 if (localMode === 'dark') {
 
   document.documentElement.setAttribute('data-dark-mode', '');
+  document.documentElement.setAttribute('data-bs-theme', 'dark');
 
 }
